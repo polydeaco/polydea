fa23404a4f1df32d163b329465035a0c7de29864
polydeaco <62583481+polydeaco@users.noreply.github.com>
2020-03-24 22:33:47 -0400

    authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ExploreComponent} from './pages/explore/explore.component'
 import { MusicComponent } from './pages/music/music.component'
 import { VideoComponent } from './pages/video/video.component'
 import { MyLibraryComponent } from './pages/my-library/my-library.component'
+import { AuthGuard } from 'src/auth.guard';
 
 const routes: Routes = [
   {
@@ -27,6 +28,7 @@ const routes: Routes = [
   {
     path:'library',
     component: MyLibraryComponent,
+    canActivate: [AuthGuard]
   }      
    
 ];
